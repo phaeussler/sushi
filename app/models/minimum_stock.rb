cc1980261afb9b0eff29fd795ea37caad5379568
joaquincastillo <jacastillo@uc.cl>
2019-04-29 20:47:06 -0300

    archivos faltantes

---
+++ app/models/minimum_stock.rb
@@ -0,0 +1,2 @@
+class MinimumStock < ApplicationRecord
+end
---
+++ test/models/minimum_stock_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MinimumStockTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
