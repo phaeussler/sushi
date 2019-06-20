442ba8cb6eb62adab42c41e1b4b356f8030bf3d7
Sebastian Manriquez <slmanriquez@uc.cl>
2019-06-19 20:02:07 -0400

    Jobs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,10 @@ class PagesController < ApplicationController
       puts "YA SE EJECUTO"
     else
       @@first_execution = true
-      # handler = Handler.new
-      # handler.satisfy_inventory_level1_job
-      # handler.satisfy_inventory_level2_job
-
-
+      handler = Handler.new
+      handler.satisfy_inventory_level1_job
+      handler.satisfy_inventory_level2_job
+      handler.satisfy_inventory_level1_gropus
 
     end
   end
