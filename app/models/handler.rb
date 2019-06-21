a7a65597dcfb9b2feb7ce7b93fb796d4e8ebc008
Sebastian Manriquez <slmanriquez@uc.cl>
2019-06-21 08:23:51 -0400

    Balancing jobs

--- app/models/handler.rb
+++ app/models/handler.rb
@@ -15,48 +15,48 @@ class Handler < CheckController
     end
     self.empty_reception()
   end
-  handle_asynchronously :empty_reception, :run_at => Proc.new {2.minutes.from_now }
+  handle_asynchronously :empty_reception, :run_at => Proc.new {2.minutes.from_now}
 
   def oc_pendientes
     puts "------------- Ordenes Pendientes job ------------".green
     pendientes()
     self.oc_pendientes()
   end
-  handle_asynchronously :oc_pendientes, :run_at => Proc.new {16.minutes.from_now }
+  handle_asynchronously :oc_pendientes, :run_at => Proc.new {16.minutes.from_now}
 
   def ordenes_de_compra_ftp
     puts "------------- Buscar Ordenes de Compra job ------------".green
     execute_ftp
     self.ordenes_de_compra_ftp
   end
-  handle_asynchronously :ordenes_de_compra_ftp, :run_at => Proc.new {3.minutes.from_now }
+  handle_asynchronously :ordenes_de_compra_ftp, :run_at => Proc.new {3.minutes.from_now}
 
   def satisfy_inventory_level1_groups_job
     puts "------------- Satisfy Inventory Level 1 Groups job ------------".green
     satisfy_inventory_level1_groups()
     self.satisfy_inventory_level1_groups_job()
   end
-  handle_asynchronously :satisfy_inventory_level1_groups_job, :run_at => Proc.new {4.minutes.from_now }
+  handle_asynchronously :satisfy_inventory_level1_groups_job, :run_at => Proc.new {4.minutes.from_now}
 
   def satisfy_inventory_level1_job}
     puts "------------- Satisfy Inventory Level job ------------".green
     satisfy_inventory_level1()
     self.satisfy_inventory_level1_job
   end
-  handle_asynchronously :satisfy_inventory_level1_job, :run_at => Proc.new {30.minutes.from_now }
+  handle_asynchronously :satisfy_inventory_level1_job, :run_at => Proc.new {30.minutes.from_now}
 
   def satisfy_inventory_level2_job
     puts "------------- Satisfy Inventory Level 2 job ------------".green
     satisfy_inventory_level2()
     self.satisfy_inventory_level2_job
   end
-  handle_asynchronously :satisfy_inventory_level2_job, :run_at => Proc.new {9.minutes.from_now }
+  handle_asynchronously :satisfy_inventory_level2_job, :run_at => Proc.new {9.minutes.from_now}
 
   def arrocero
     puts "------------- Arrocero job ------------".green
     fabricar_producto(10, 1101, 'despacho')
     self.arrocero
   end
-  handle_asynchronously :arrocero, :run_at => Proc.new {6.minutes.from_now }
+  handle_asynchronously :arrocero, :run_at => Proc.new {6.minutes.from_now}
 
 end
